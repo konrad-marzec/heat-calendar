5a847cd74a0bb5bc1d2ed14b6745a1b4a072e164
Konrad Marzec <konrad.marzec007@gmail.com>
2023-06-23 16:36:35 +0200

    remove old page

--- apps/docs/src/stories/HeatCalendar.tsx
+++ apps/docs/src/stories/HeatCalendar.tsx
@@ -1,10 +1,6 @@
 import React, { ComponentProps } from 'react';
 import { HeatCalendar as CoreHeatCalendar } from 'core';
 
-const COLORS = ['#CCDBDC', '#9AD1D4', '#C7F9CC', '#DBFF76', '#B6DC76', '#80CED7', '#57CC99', '#007EA7', '#003249'];
-const ORANGE_COLORS = ['#7a7672', '#e0c599', '#e2a458', '#d46b08', '#873800'];
-const GREEN_COLORS = ['#5e5e56', '#7cb305', '#5b8c00', '#3f6600'];
-
 const container = {
   padding: 16,
   fontSize: 6,
